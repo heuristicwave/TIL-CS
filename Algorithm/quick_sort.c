c735f6a70c489f2c6f038231e0ddf952551035fc
heuristicwave <mtinj16@gmail.com>
2019-07-04 14:59:18 +0900

    Create quick_sort.c

---
+++ Algorithm/quick_sort.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define swap(type, x, y) do { type t = x; x = y; y = t; } while(0)
+
+void quick (int a[], int left, int right)
+{
+	int left_cursor = left;
+	int right_cursor = right;
+	int pivot = a[(left_cursor + right_cursor) / 2];	// Select center pivot
+	
+	// Divide array a by pivot
+	do {
+		while(a[left_cursor] < pivot) left_cursor++;	// Move one space to the right after comparing with pivot
+		while(a[right_cursor] > pivot) right_cursor--;  // Move one space to the left after comparing with pivot
+		if(left_cursor <= right_cursor) {
+			swap(int, a[left_cursor], a[right_cursor]);
+			left_cursor++;
+			right_cursor--;
+		}
+	} while(left_cursor <= right_cursor);	// After crossing cursors, complete
+	
+	// Recursive calls to repartition left and right groups
+	if (left < right_cursor) quick(a, left, right_cursor);
+	if (left_cursor < right) quick(a, left_cursor, right);	
+}
+
+int main(void){
+	int i, nx;
+	int *x;		// Pointer to array first element
+	
+	puts("Quick Sort");
+	printf("요소 개수 : ");
+	scanf("%d", &nx);
+	
+	x = calloc(nx, sizeof(int));
+	
+	for(i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+	
+	quick(x, 0, nx - 1);
+	puts("오름차순 정렬");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+	
+	free(x);
+	
+	return 0;	
+}
