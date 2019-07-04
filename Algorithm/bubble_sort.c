6f39bc3d5542ee9c8703df5bdd9f8c215eb7b7ad
heuristicwave <mtinj16@gmail.com>
2019-07-04 14:59:10 +0900

    Create bubble_sort.c

---
+++ Algorithm/bubble_sort.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define swap(type, x, y) do { type t = x; x = y; y = t; } while(0)
+
+void bubble(int a[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++) {
+		for (j = n - 1; j > i; j--)
+			if (a[j - 1] > a[j])
+				swap(int, a[j - 1], a[j]);
+	}
+}
+
+int main(void)
+{
+	int i, nx;
+	int *x;
+
+	puts("버블정렬");
+	printf("요소 개수 : ");
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int)); // 요소의 개수가 int형 배열을 생성
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+
+	bubble(x, nx);
+
+	puts("오름차순으로 정렬");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+
+	free(x);
+
+	return 0;
+}
