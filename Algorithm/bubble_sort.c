e28e8d1fc7732f0a18777bd90eecebc661f462af
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2019-07-05 10:49:55 +0900

    Performance improvements

--- Algorithm/bubble_sort.c
+++ Algorithm/bubble_sort.c
@@ -6,9 +6,13 @@ void bubble(int a[], int n)
 {
 	int i, j;
 	for (i = 0; i < n - 1; i++) {
+		int exchg = 0;		// Number of exchanges attempted on the pass
 		for (j = n - 1; j > i; j--)
-			if (a[j - 1] > a[j])
+			if (a[j - 1] > a[j]){
 				swap(int, a[j - 1], a[j]);
+				exchg++;
+			}
+		if (exchg == 0 ) break;		// If the exchange has not been performed, the alignment stops.
 	}
 }
 
