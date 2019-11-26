525cd691fc07e65dd5ea7ca0b34a15a46d1b624e
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2019-11-26 14:59:10 +0900

    Create link.c

---
+++ SystemProgramming/src/link.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define TARGET_FILE "hello_world"
+
+int main (int argc, char *argv[])
+{
+    if (link(TARGET_FILE, "hello_by_link")){
+	printf("link() fail\n");
+	return -1;
+    }
+
+    if (symlink(TARGET_FILE, "hello_by_symlink")){
+	printf("symlink() fail\n");
+	return -1;
+    }
+
+    return 0;
+}
