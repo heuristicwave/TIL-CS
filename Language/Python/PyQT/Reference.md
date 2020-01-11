a2ce246ea28706b35336d51574a2e2e7eee678c0
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-01-11 22:05:20 +0900

    pyqt practice

---
+++ Language/Python/PyQT/Reference.md
@@ -0,0 +1,20 @@
+## Reference
+
+[초보자를 위한 파이썬 gui 프로그래밍 - PyQt5](https://wikidocs.net/book/2944)
+[PyQt5 Tutorial](http://codetorial.net/pyqt5/index.html)
+
+
+**anaconda**
+
+```python
+designer # qt designer 실행
+```
+
+
+
+**windows10 powershell**
+
+```powershell
+pip install pyqt5
+```
+
--- Language/Python/PyQT/untitled.py
+++ Language/Python/PyQT/untitled.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
@@ -10,7 +12,29 @@ form_class = uic.loadUiType("untitled.ui")[0]
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
-        self.setupUi(self)
+        self.setupUi(self) # visible UI
+
+        self.initUI()
+    
+    def initUI(self):
+
+        btn = QPushButton('Quit', self)
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+        self.setWindowTitle('Reverse Othello')
+        self.setWindowIcon(QIcon('Othello icon.png'))
+        self.setGeometry(700, 800, 800, 700)
+        self.center()
+        self.show()
+    
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
