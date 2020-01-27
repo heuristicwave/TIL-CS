c39537701d73fab328d375f5994db0df9c2278e4
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-01-27 19:34:10 +0900

    add extract_inedeed_jobs

--- Language/Python/Building-a-Job-Scrapper/indeed.py
+++ Language/Python/Building-a-Job-Scrapper/indeed.py
@@ -21,6 +21,19 @@ def extract_indeed_pages():
 
 
 def extract_indeed_jobs(last_page):
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code)
+    jobs = []
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, 'html.parser')
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        title = result.find("div", {"class": "title"}).find("a")["title"]
+        company = result.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = (str(company_anchor.string))
+        else:
+            company = (str(company.string))
+        company = company.strip()
+        print(title, company)
+    return jobs
