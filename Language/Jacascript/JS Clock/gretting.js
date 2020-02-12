e940172216abd831acd8258bf4cda927cc7b31ba
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-02-13 02:27:43 +0900

    add using localStorage

--- Language/Jacascript/JS Clock/gretting.js
+++ Language/Jacascript/JS Clock/gretting.js
@@ -5,6 +5,23 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  // console.log(currentValue);
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +31,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
