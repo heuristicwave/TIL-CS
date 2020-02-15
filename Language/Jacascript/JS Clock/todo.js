515558a14c6ffacf91b18f22cb8cc70d2d3f8757
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2020-02-15 21:57:56 +0900

    add deleteToDo

--- Language/Jacascript/JS Clock/todo.js
+++ Language/Jacascript/JS Clock/todo.js
@@ -4,10 +4,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function deleteToDo(event) {
+  // console.dir(event.target.parentNode);
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   const span = document.createElement("span");
   span.innerText = text;
   li.appendChild(delBtn);
